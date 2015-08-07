f80bd82b6f41bd62383250aff71e46b81f4e00fd
Rahul Ranjan <rahul.rrixe@gmail.com>
2015-08-08 03:54:38 +0530

    unittest for the index view

---
+++ tests/test_fresque_views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import pkg_resources
+
+import unittest
+import shutil
+import sys
+import os
+
+import json
+from mock import patch
+
+sys.path.insert(0, os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..'))
+
+import fresque.lib
+import tests
+
+
+class FresqueFlaskApptests(tests.Modeltests):
+    """ Tests for flask app controller of pagure """
+
+    def setUp(self):
+        """ Set up the environnment, ran before every tests. """
+        super(FresqueFlaskApptests, self).setUp()
+
+        fresque.APP.config['TESTING'] = True
+        fresque.SESSION = self.session
+
+        fresque.APP.config['GIT_DIRECTORY_PATH'] = tests.HERE
+        self.app = fresque.APP.test_client()
+
+    def test_index(self):
+        """ Test the index endpoint. """
+
+        output = self.app.get('/')
+        self.assertEqual(output.status_code, 200)
+        self.assertTrue('<h2>Recent activity</h2>' in output.data)
+
+        tests.create_packages(self.session)
+
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(FresqueFlaskApptests)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
